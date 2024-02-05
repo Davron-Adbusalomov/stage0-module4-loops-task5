18c03ff7b509bb5e2f58b1af503964c9d9ce05bd
Davron-Adbusalomov <115224119+Davron-Adbusalomov@users.noreply.github.com>
2024-02-05 23:56:55 +0500

    Update Triangle.java

--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -2,6 +2,17 @@ package school.mjc.stage0.loops.task5;
 
 public class Triangle {
     public void printTriangle(int cathetusLength) {
+         if (cathetusLength <= 0) {
+            System.out.println("Invalid size. Please provide a positive size.");
+            return;
+        }
 
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = 1; j <= i; j++) {
+                // Print '8' for each position in the triangle
+                System.out.print("8");
+            }
+            System.out.println(); // Move to the next line after each row
+        }
     }
 }
